7ee081fbe4c44dc681c3992786efc33472dd6569
rainbowjlinux <rainbowjlinux@gmail.com>
2017-10-17 20:40:55 +0800

    Optimize code
    1. Add exception handling
    2. use format function
    
    Signed-off-by: rainbowjlinux <rainbowjlinux@gmail.com>

--- Challenge1/calculate.py
+++ Challenge1/calculate.py
@@ -1,36 +1,41 @@
 #!/usr/bin/env python3
 import sys
 
-#print (sys.argv)
-
-def salcal():
+def calculator():
     salary=[1500, 4500, 9000, 35000, 55000, 80000]
     
     taxrate=[0.03, 0.1, 0.2, 0.25, 0.3, 0.35, 0.45]
     
     desal=[0, 105, 555, 1005, 2755, 5505, 13505]
     
-    r = int(sys.argv[1])
-    
-    if (r < 3500):
-        print ("%.2f" % 0)
+    try:
+        if len(sys.argv) != 2:
+            raise ValueError
+        r = int(sys.argv[1])
+        print ("Your salary is {}".format(sys.argv[1], ".2f"))
+    except:
+        print ("Parameter Error")
+        return
     else:
-        x = r - 3500
-        for i in range(6):
-            #print (i)
-            if (x-salary[i] <= 0):
-                y = taxrate[i]
-                z = desal[i]
-                #print(x, y, z)
-                print ("%.2f" % (x * y - z))
-                return
-        y = taxrate[i+1]
-        z = desal[i+1]
-        #print(x, y, z)
-        print ("%.2f" % (x * y - z))
+        if (r < 3500):
+            print ("You need to pay 0.00")
+        else:
+            x = r - 3500
+            for i in range(6):
+                if (x-salary[i] <= 0):
+                    y = taxrate[i]
+                    z = desal[i]
+                    print ("You need to pay {}".format(x * y - z, ".2f"))
+                    return
+            y = taxrate[i+1]
+            z = desal[i+1]
+            print ("You need to pay {}".format(x * y - z, ".2f"))
+    finally:
+        print ("\nThks.")
 
 def main():
-    salcal()
+    calculator()
+    print ("This is a salary tax calculator by wjl\nUsage: ./calculator.py $[INT]")
 
 if __name__ == '__main__':
     main()
