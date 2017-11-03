19862c2893c3a8fe250e11918055c9a6750d38be
wangjinlaishuai <wangjinlaishuai@163.com>
2017-11-03 21:37:31 +0800

    Practice the property decorator

--- advance/decrator.py
+++ advance/decrator.py
@@ -9,15 +9,57 @@
 
 from datetime import datetime
 
-if __name__ == '__main__':
+BIRTH_TABLE = {
+        'xiaoshumiao' : 2014,
+        'xiaopingguo' : 2015
+        }
 
-    def log(func):
-        def decrator(*args, **kwargs):
-            print(func.__name__,'has been called at',datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-            return func(*args, **kwargs)
-        return decrator
+class Baby(object):
+    '''
+    def __init__(self, year):
+        self._birth = year
+    '''
+    def _baby_birth(self, name):
+        return BIRTH_TABLE[name]
+
+    @property
+    def xiaoshumiao_birth(self):
+        return self._baby_birth('xiaoshumiao')
+
+    @property
+    def xiaopingguo_birth(self):
+        return self._baby_birth('xiaopingguo')
+
+    @property
+    def birth(self):
+        return self._birth
+    @birth.setter
+    def birth(self, value):
+        self._birth = value
+
+    @property
+    def age(self):
+        return 2017 - self._birth
+
+def log(func):
+    def decrator(*args, **kwargs):
+        print(func.__name__,'has been called at',datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+        return func(*args, **kwargs)
+    return decrator
+
+def main():
     @log
     def add(x, y):
         return x+y
 
-    print(add(1,2))
+    add(1,2)
+
+    bb = Baby()
+    print(bb.xiaoshumiao_birth)
+    print(bb.xiaopingguo_birth)
+
+    bb.birth=1987
+    print(bb.age)
+
+if __name__ == '__main__':
+    main()
